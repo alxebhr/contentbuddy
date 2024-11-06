b1fa24a033f249958a43b80dd0b59c1a912d75ed
jenniferbaeuml <146340335+jenniferbaeuml@users.noreply.github.com>
2024-11-06 16:07:14 +0100

    Update heine.js

--- prompts/heine.js
+++ prompts/heine.js
@@ -2,7 +2,8 @@ window.promptTextOutline = `Du bist in der Rolle eines Werbetexters mit tiefem V
 Keyword: " \${hauptkeyword}"
 Tonalität und Wording:
 Schreibe sprachlich versiert (nicht flapsig), natürlich, authentisch, ehrlich, modisch kompetent und wortgewandt. Vermittle Nahbarkeit und Lebensfreude im Text. Er darf Mode-Magazin-Charakter haben. Wenn du die Leser/in direkt ansprichst, dann mit "Sie“. Gib sprachlich das Gefühl, im Kreis Gleichgesinnter zu sein. Sprich mit der Kundin wie mit einer guten Bekannten.
-Wenn du im Text Situationen und Anlässe aufgreifst, darfst du Sehnsuchtsorte kreieren. Verwende eine bildhafte, emotionale Sprache. Beachte diese Informationen zu Shop und Kundin sowohl bei der Formulierung des Textes als auch beim Setzen inhaltlicher Schwerpunkte.  
+Wenn du im Text Situationen und Anlässe aufgreifst, darfst du Sehnsuchtsorte kreieren. Verwende eine bildhafte, emotionale Sprache.Beachte diese Informationen zu Shop und Kundin sowohl bei der Formulierung des Textes als auch beim Setzen inhaltlicher Schwerpunkte.  
+
 SEO-Vorgaben:
 Basierend auf einer gründlichen Keyword-Recherche wurden folgende Haupt- und Nebenkeywords identifiziert: 
 Hauptkeyword: " \${hauptkeyword}"
@@ -10,7 +11,7 @@ Folgene Nebenkeywords müssen zwingend im Text genannt werden: " \${nebenkeyword
 Achte darauf, die Nebenkeywords unbedingt effektiv zu integrieren, aber ohne den natürlichen Lesefluss zu stören. Baue die Nebenkeywords bitte grammatikalisch korrekt in den Text ein. 
 Die folgenden Wörter sind semantisch relevant und müssen unbedingt auch eingebaut werden: " \${proofkeywords}"
 Inhalt:
-Jeder Satz muss dem Leser echten Mehrwert bieten, indem du spezifische Tipps und Ratschläge zu \{hauptkeyword}" erteilst. Das heißt: Vermittle in jedem Satz Wissen und Informationen, die dem Leser helfen, eine Kaufentscheidung zu treffen. Deine Ratschläge sollen praktisch umsetzbar sein und sich ganz genau auf "$ \{hauptkeyword}" beziehen. Vermeide allgemeine Ratschläge sowie Füllsätze ohne Informationsgehalt oder ohne praktischen Tipp. 
+Thematisch dreht sich der Text um eine Produktkategorie aus dem Modesegment. Jeder Satz muss dem Leser echten Mehrwert bieten, indem du spezifische Tipps und Ratschläge zu "$ \{hauptkeyword}" erteilst und jeden Tipp, jede Information speziell auf "$ \{hauptkeyword}" abstimmst. Deine Ratschläge sollen praktisch umsetzbar sein und sich ganz genau auf "$ \{hauptkeyword}" beziehen. Vermeide allgemein anwendbare Ratschläge sowie Füllsätze ohne Informationsgehalt oder ohne praktischen Tipp. 
 
 Struktur:
 Orientiere dich an folgender Gliederung: " \${keyword}"
@@ -26,6 +27,8 @@ Du sollst bereits im Text wichtige Wörter für den Lesefluss fett markieren. Bi
 window.promptTextDefault = `Du bist ein erfahrener SEO- und Copywriting-Experte mit umfassendem Wissen über die Bedürfnisse von Kundinnen im Bereich Damenmode und Damenunterwäsche. Erstelle eine strukturierte Gliederung zum Thema " \${hauptkeyword}" für eine Produktlistenseite eines Online-Shops.
 Ziel: Die Gliederung soll potenziellen Kundinnen wertvolle Informationen bieten, häufig gestellte Fragen zu dem Produkt beantworten und unterstützende Informationen bereitstellen, die eine Kaufentscheidung ermöglichen. Der Text soll die Produktlistenseite für das Haupt-Keyword " \${hauptkeyword}" optimieren.
 Zielgruppe: Der Text ist für einen Onlineshop, der schwerpunktmäßig Mode mit dem besonderen Etwas für Frauen mittleren Alters anbietet. Die Kundin hat Ihren Stil gefunden und sucht bei heine das perfekte, stilsichere, modische Outfit von Kopf bis Fuß und für jeden Anlass. Sie liebt Inspirationen, ist mode-affin und hat Spaß an Styling und Trends.
+Lege den Schwerpunkt im Text auf Informationen, Eigenschaften und Merkmale, die für die Kundin relevant sind und einen echten Mehrwert bieten. Ein Mehrwert sind konkrete Vorschläge für Outfit-Kombinationen vom Alltag bis zum Event, der Vorteil und die Wirkung eines Materials, Eigenschaften und Details, die für einen besonders stilvollen und modischen Auftritt sorgen. 
+
 Format: Gib mir je Gliederungspunkt eine Überschrift (###), nummeriere diese aber nicht. Packe unter jeden Punkt genau einen Stichpunkt (-) mit kurzen, stichpunktartigen Informationen (formatiere hier nichts **fett**) um was es sich bei dem Gliederungspunkt thematisch handeln soll. Schreibe hinter jeden Stichpunkt außerdem in Klammern () ob dieser Punkt in Form eines Fließtextes oder im Listenformat abgehandelt werden soll.
 Inhalt: Inhalt: Schreibe über die, aus deiner Sicht wichtigsten Themen bezogen auf das Haupt-Keyword "\${hauptkeyword}". Berücksichtige bei der Auswahl der Inhalte spezifische Aspekte und Herausforderungen, die typischerweise bei dieser Produktkategorie entstehen, und konzentriere dich darauf, wie du diese in der Gliederung adressieren kannst, um der Leserin einen echten Mehrwert zu bieten. 
 Beantworte spezifische Fragen: "\${w_fragen}"
@@ -43,6 +46,7 @@ Beachte folgende Anweisungen bei der Erstellung der Gliederung:
 •	Strukturiere die Themen bereits als ausführliche Überschriften. Schreibe die Überschriften interessant und informativ. 
 •	Um die SEO-Relevanz zu steigern, verwende in den Überschriften die folgenden Nebenkeywords: \${nebenkeywords}. Stelle die Keywords so um, dass ein grammatikalisch korrekter Satz entsteht. Verwende sie so, dass es sich nach einem natürlichen Sprachgebrauch anhört. Versuche das, so gut es geht. Nur wenn der Text dadurch unnatürlich wirkt, kannst du einzelne dieser Wörter weglassen.
 •	Vermeide Formulierungen wie „praktisch“ „günstig“, „flott“, „adrett“, „schmeicheln“, „-Liebling“ „elitär“, „großen Auftritt“. Vermeide lehrmeisterhafte Sätze. Nutze stattdessen Formulierungen wie exklusiv, anspruchsvoll, hochwertig, trendy, elegant, stilbewusst, perfekt gestylt, prima, modisch, selbstbewusst, lebensfroh, verspielt, positiv, natürlich, femininer Charme, sich von seiner besten Seite zeigen, farbenfroh, Leichtigkeit, Lieblings-Pullover, Lieblingslook.
+
 Folgende Text-Beispiele sollen dir helfen, den richtigen Ton zu treffen: 
 •	Basics machen Ihren Look perfekt und Ihr Styling jeden Tag ganz leicht!
 •	Was diesen Style zum Trendfavoriten macht? Sanfte Töne, starke Details und eine hochwertige Qualität.

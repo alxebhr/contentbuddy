a31c77417376a2b8e80c2f29a9a6075277e65ed5
jenniferbaeuml <146340335+jenniferbaeuml@users.noreply.github.com>
2025-03-04 08:19:06 +0100

    Update contentBuddyTest.js

--- contentBuddyTest.js
+++ contentBuddyTest.js
@@ -7,7 +7,7 @@
     let firstTime = true; // Track the first time the text is inserted
     let initialized = false; // Neues Flag: verhindert mehrfache Initialisierung
 
-    function insertTextAndSend(hauptkeyword, keyword, nebenkeywords,  proofkeywords = '', w_fragen, outlineText = "") {
+    function insertTextAndSend(hauptkeyword, keyword, nebenkeywords, proofkeywords, w_fragen, outlineText = "") {
         // Versuche zuerst den Quill-Editor zu finden
         let quillEditorContainer = document.querySelector('.v-ql-textarea.ql-container');
         console.log('🔍 Suche nach Quill-Editor:', quillEditorContainer);
